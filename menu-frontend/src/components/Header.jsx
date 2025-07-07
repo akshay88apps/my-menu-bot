908db310578fcc0507a7dc32fda610af74c19186
akshay88apps <akshay88apps@gmail.com>
2025-07-07 21:50:45 +0530

    Delete menu-frontend directory

--- menu-frontend/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-// -------------------- Header.jsx --------------------
-export const Header = ({ clearOrder }) => (
-  <header className="p-4 bg-white shadow w-full text-center">
-    <h1 className="text-xl font-semibold">Welcome to your own restaurant - XXX</h1>
-    <button className="text-blue-600 underline mt-2" onClick={() => window.open("/menu.pdf", "_blank")}>View Full Menu</button>
-  </header>
-);
